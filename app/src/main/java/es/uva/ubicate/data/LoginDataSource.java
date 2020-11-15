2370f5ba3d5e66502677a67c92b1d77fa495344d
javgato <javier.gaton@alumnos.uva.es>
2020-11-15 13:32:56 +0100

    auth email password

--- app/src/main/java/es/uva/ubicate/data/LoginDataSource.java
+++ app/src/main/java/es/uva/ubicate/data/LoginDataSource.java
@@ -1,26 +1,61 @@
 package es.uva.ubicate.data;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
 import es.uva.ubicate.data.model.LoggedInUser;
 
 import java.io.IOException;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
  */
 public class LoginDataSource {
 
+    private String TAG = "LoginDataSource";
+
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+
     public Result<LoggedInUser> login(String username, String password) {
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
 
         try {
             // TODO: handle loggedInUser authentication
             // AUTENTIFICACION SE HACE AQUI
             // Pongo esto para poder observar que pasa cuando no funciona
-            if(username.equals("admin")) {
-                LoggedInUser fakeUser =
-                        new LoggedInUser(
-                                java.util.UUID.randomUUID().toString(),
-                                "Jane Doe");
-                return new Result.Success<>(fakeUser);
+            //Task<AuthResult> task = mAuth.createUserWithEmailAndPassword(username, password);
+            Task<AuthResult> task = mAuth.signInWithEmailAndPassword(username, password);
+            /*.addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    Log.d(TAG, "signIn:onComplete:" + task.isSuccessful());
+                    //hideProgressBar();
+
+                    if (task.isSuccessful()) {
+                        onAuthSuccess(task.getResult().getUser());
+                    } else {
+                        //Toast.makeText(SignInActivity.this, "Sign In Failed",
+                        //        Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });*/
+            while(!task.isComplete()){}
+            if(task.isSuccessful()) {
+                FirebaseUser fUser = task.getResult().getUser();
+                LoggedInUser usuario = new LoggedInUser(fUser.getUid(),fUser.getDisplayName());
+                return new Result.Success<>(usuario);
             }else{
                 return new Result.Error(new IllegalArgumentException("Error de username"));
             }
