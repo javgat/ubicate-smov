c45e38a3a5859266c5a637093a273c10186e9513
javgato <javier.gaton@alumnos.uva.es>
2020-12-14 01:38:05 +0100

    Inicio de cambios para localizacion

--- app/src/main/java/es/uva/ubicate/ui/map/MapsFragment.java
+++ app/src/main/java/es/uva/ubicate/ui/map/MapsFragment.java
@@ -2,9 +2,20 @@ package es.uva.ubicate.ui.map;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,13 +26,19 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import es.uva.ubicate.DrawerActivity;
 import es.uva.ubicate.R;
 
 public class MapsFragment extends Fragment {
 
     private final String TAG = "MapsFragment";
+
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -38,7 +55,28 @@ public class MapsFragment extends Fragment {
         public void onMapReady(GoogleMap googleMap) {
             LatLng vall = new LatLng(41.6524076, -4.7246467);
             googleMap.addMarker(new MarkerOptions().position(vall).title("Marcador en Valladolid"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(vall));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(vall, 1));
+            Marker markUser = googleMap.addMarker(new MarkerOptions().position(vall).title("Tu ubicacion"));
+            markUser.setVisible(false);
+            locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+            /*
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location loc) {
+                    Log.d(TAG, "Location cambiada");
+                    LatLng locUser = new LatLng(loc.getLatitude(), loc.getLongitude());
+                    markUser.setPosition(locUser);
+                    markUser.setVisible(true);
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLng(locUser));
+                }
+            };*/
+            locationListener = null;
+            /*if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                OnGPS();
+            }else {
+                getLocation();
+            }*/
+
         }
     };
 
@@ -58,6 +96,37 @@ public class MapsFragment extends Fragment {
 
         if (mapFragment != null) {
             mapFragment.getMapAsync(callback);
+        }
+    }
+
+    private void OnGPS() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setMessage("Enable GPS").setCancelable(false).setPositiveButton("Yes", new  DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+
+
+    private void getLocation() {
+        int REQUEST_LOCATION = 1;
+        Location locationGPS;
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
+        }else{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 0, locationListener);
         }
     }
 }
