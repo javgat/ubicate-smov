386b8569cbde9b13060e27a2799893b311094464
javgato <javier.gaton@alumnos.uva.es>
2020-12-17 20:23:41 +0100

    peq cambios para la presentacion

--- app/src/main/java/es/uva/ubicate/DrawerActivity.java
+++ app/src/main/java/es/uva/ubicate/DrawerActivity.java
@@ -94,7 +94,7 @@ public class DrawerActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_maps, R.id.nav_chat, R.id.nav_domain, R.id.nav_profile)
+                R.id.nav_home, R.id.nav_maps, R.id.nav_chat, R.id.nav_domain, R.id.nav_profile, R.id.nav_logout)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
@@ -206,7 +206,7 @@ public class DrawerActivity extends AppCompatActivity {
             case R.id.action_settings:
                 //startActivity(new Intent(this, SettingsActivity.class));
                 return true;
-            case R.id.action_logout:
+            /*case R.id.action_logout:
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setMessage(R.string.dialog_logout)
                         .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
@@ -224,7 +224,7 @@ public class DrawerActivity extends AppCompatActivity {
                 // Create the AlertDialog object and return it
                 AlertDialog dialog = builder.create();
                 dialog.show();
-                return true;
+                return true;*/
             default:
                 return false;
         }
