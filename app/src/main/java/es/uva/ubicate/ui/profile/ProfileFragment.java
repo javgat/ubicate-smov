38372513134d3fcd5cb17232d3ff0f332f0dc5d6
javgato <javier.gaton@alumnos.uva.es>
2020-12-28 14:37:21 +0100

    Permission flow bugs fixed

--- app/src/main/java/es/uva/ubicate/ui/profile/ProfileFragment.java
+++ app/src/main/java/es/uva/ubicate/ui/profile/ProfileFragment.java
@@ -1,8 +1,10 @@
 package es.uva.ubicate.ui.profile;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
@@ -24,9 +26,13 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -57,6 +63,7 @@ public class ProfileFragment extends Fragment {
     private final String TAG = "ProfileFragment";
 
     private static final int PICK_IMAGE = 100;
+    private static final int REQ_STORAGE = 200;
     private View raiz;
 
     private void updateValuesUI(View root){
@@ -288,8 +295,29 @@ public class ProfileFragment extends Fragment {
         }
     }
 
-    private void tryEditarImage(View root){
-        openGallery();
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case REQ_STORAGE:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    tryEditarImage();
+                }  else {
+                    Toast.makeText(getContext(), R.string.almacenamiento_no_activa, Toast.LENGTH_LONG).show();
+                }
+                return;
+        }
+        // Other 'case' lines to check for other
+        // permissions this app might request.
+    }
+
+    private void tryEditarImage(){
+        if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_DENIED){
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},REQ_STORAGE);//??
+        }else
+            openGallery();
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -329,7 +357,7 @@ public class ProfileFragment extends Fragment {
         button_image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tryEditarImage(root);
+                tryEditarImage();
             }
         });
 
